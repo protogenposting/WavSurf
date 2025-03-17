51aa47858c2fd82fab2202417254d57efe97443d
Carson King <86360309+protogenposting@users.noreply.github.com>
2025-03-17 12:06:32 -0400

    docs

--- index/navbar/navbarScript.js
+++ index/navbar/navbarScript.js
@@ -1,5 +1,9 @@
+//adding this script to any html file loads the navbar on top, do this on every page that's accessible to every user!
+
+//create the navbar
 const nav = document.createElement("nav")
 
+//load the navbar
 fetch('http://localhost:3000/navbar', {
     method: "GET",
     headers: {
@@ -8,6 +12,7 @@ fetch('http://localhost:3000/navbar', {
 })
 .then(res=>res.text())
 .then(data=>{
+    //add the navbar to the page
     nav.innerHTML=data
     nav.setAttribute("style","top: 0px;")
     nav.setAttribute("class","navbar")
